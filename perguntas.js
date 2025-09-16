796b2cd581d1e725c992cc52f223c6699506564d
joao916 <githubjoaobiancato@gmail.com>
2025-09-16 09:28:57 -0300

    16/09/25

--- perguntas.js
+++ perguntas.js
@@ -1,61 +1,51 @@
 criaCartao(
 'brazil',
 'Qual é o maior estado do Brasil em termos de área territorial?',
-'resposta'
+'O Amazonas é o maior estado brasileiro em área territorial, com mais de 1,5 milhão de km².'
 )
 
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'brazil',
+'Quem foi o primeiro presidente do Brasil após a Proclamação da República em 1889?',
+'O primeiro presidente do Brasil após a Proclamação da República foi Marechal Deodoro da Fonseca, que assumiu em 1889. '
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'brazil',
+'O que significa o termo "Brasil" em relação ao nome do país? ',
+'O nome "Brasil" vem do pau-brasil, uma árvore nativa da região costeira, cujo extrato era muito utilizado para a produção de tintura vermelha.'
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'brazil',
+'Quais são as cores da bandeira brasileira?',
+'As cores da bandeira brasileira são: Verde, Amarelo, Azul.
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'PARANÁ',
+'Qual é a capital do Paraná?',
+'Curitiba'
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'Paraná',
+'Que festa anual celebra a cultura ucraniana em Prudentópolis?',
+'A Festa Nacional do Strogonoff.'
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'paraná',
+'Qual é o nome do festival de teatro famoso de Curitiba?',
+' O Festival de Teatro de Curitiba.'
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'paraná',
+'Que tipo de dança típica do litoral paranaense é Patrimônio Imaterial do estado?'
+'A Dança do Fandango'
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'paraná',
+'Que grande área verde em Curitiba homenageia a comunidade japonesa?'
+'O Jardim Botânico de Curitiba.'
 )
 criaCartao(
-'categoria',
-'pergunta',
-'resposta'
-)
-criaCartao(
-'categoria',
-'pergunta',
-'resposta'
-)
-criaCartao(
-'categoria',
-'pergunta',
-'resposta'
+'paraná',
+'Qual é o principal rio que forma as Cataratas do Iguaçu?',
+' O Rio Iguaçu.'
 )
